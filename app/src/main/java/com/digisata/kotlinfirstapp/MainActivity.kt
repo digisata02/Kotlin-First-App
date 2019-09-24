20805acfe5badc7dbacbb5c703f0973faaa05eef
digisata02 <55724786+digisata02@users.noreply.github.com>
2019-09-24 09:15:26 +0700

    Revert "added new comment"

--- app/src/main/java/com/digisata/kotlinfirstapp/MainActivity.kt
+++ app/src/main/java/com/digisata/kotlinfirstapp/MainActivity.kt
@@ -36,8 +36,6 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener { v ->
             seekBar.progress = 0
             seekBarView.animate().setDuration(500).rotationXBy(360f).translationY(textViewY)
-                                   
-            // collaboration test
         }
     }
 }
